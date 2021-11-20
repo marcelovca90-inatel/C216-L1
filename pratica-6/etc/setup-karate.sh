252d68c27c8201aa076986a01ff1d7d49df2ff38
Marcelo Cysneiros <marcelovca90@gmail.com>
2021-11-20 17:18:16 -0300

    Fix artifact name

--- pratica-6/etc/setup-karate.sh
+++ pratica-6/etc/setup-karate.sh
@@ -4,12 +4,6 @@ mvn archetype:generate \
 -DarchetypeGroupId=com.intuit.karate \
 -DarchetypeArtifactId=karate-archetype \
 -DarchetypeVersion=1.0.1 \
--DgroupId=br.inatel.c201-l1 \
+-DgroupId=br.inatel.c216-l1 \
 -DartifactId=test \
 --batch-mode
-
-rm -rf ./test/src/test/java/examples/users
-
-mkdir ./test/src/test/java/middleware/aluno
-
-touch ./test/src/test/java/middleware/aluno/aluno.feature
